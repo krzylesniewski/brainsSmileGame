8a0c889ce63cbfb8ce407fa8cc6b7b8fbf40ed18
Krzysztof Leśniewski <krzysztof.lesniewski.wroc@gmail.com>
2019-02-28 08:58:41 +0100

    game logic works

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Board from './components/Board'
 import LevelPicker from './components/LevelPicker'
 
-
+// TODO bug on level 4. Put pawn on top and see -> checking state out of boards limit
 export default class App extends Component {
     constructor(props) {
         super(props);
@@ -15,6 +15,8 @@ export default class App extends Component {
             stepLimit: 0,
             levelID: 0,
             stepNumber: 0,
+            showWinModal: false,
+            showLostModal: false,
         };
     }
 
@@ -29,11 +31,11 @@ export default class App extends Component {
         })
     };
 
+    // MAIN FUNCTION !!!!!
     handleClick(i, id) {
         const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const board = current.squares.slice();
-        // console.log(i, id);
         if (board[id] === 0) {
             board[id] = 4;
             this.stateChangeHandle(id, board);
@@ -43,97 +45,143 @@ export default class App extends Component {
                 }),
                 stepNumber: history.length,
             });
-            
-            // wywołanie sprawdzenia ktore elementy maja się zmienić
-            // złapać ID - > Spradzić pozić wszystkie kierunki
-            // jeżeli znajdziemy element o "i" 3 || 4 zwiększyć wszystkim podrodze i++;
-            //
+        }
+        this.gameStatusResolver();
+    }
+
+    componentDidUpdate() {
+        this.gameStatusResolver();
+        // TODO function to check game status [win=all state >3 , lost=move limit reach]
+    }
+
+    gameStatusResolver() {
+        // checking if all squars are smiled :D
+        const {stepLimit, history} = this.state;
+        const moveDone = history.length - 1;
+        const currentBoard = history[moveDone].squares;
+
+        // console.log(currentBoard);
+        // console.log(moveDone,currentBoard.indexOf(2));
+
+        if (currentBoard.indexOf(2) < 0 && currentBoard.indexOf(1) < 0 && moveDone > 1) {
+            console.log("Gratulacje wygrałeś");
+            // TODO conditional rendering => Model with game Win
+            // TODO block posobility to put more pawn f()
+            // this.setState({
+            //     showWinModal: true,
+            // });
+
+        } else if (moveDone >= stepLimit && moveDone > 1) {
+            console.log("Koniec gry nie masz już więcej pionków");
+            // TODO conditional rendering => Model with game lost and button try again
+            // TODO block posobility to put more pawn f()
         }
     }
 
     updateStateOfSingleSquare(id, board) {
-        if(board[id]!==0 &&board[id]<3){
+        if (board[id] !== 0 && board[id] < 3) {
             board[id]++;
         }
     }
 
+    // function to checking if we can change state of squars from giver arr
     squareNewStateFinder(arr, board) {
-        // TODO Dopisać kiedyś prawdzanie czy warto sprawdzać
-        console.log(arr);
+        // TODO add checking to do not cross game board
         let start = null;
         let startID = null;
         let end = null;
         let endID = null;
-        arr.forEach((cell,key) => {
-            if(cell.value >= 3 && start === null) {
+        arr.forEach((cell, key) => {
+            if (cell.value >= 3 && start === null) {
                 start = cell.id;
                 startID = key;
-            } else if (cell.value >= 3 && start !== null){
+            } else if (cell.value >= 3 && start !== null) {
                 end = cell.id;
                 endID = key;
             }
         });
-        if(start !== null && end !== null){
-            for(let i = startID+1; i<endID; i++){
+        if (start !== null && end !== null) {
+            for (let i = startID + 1; i < endID; i++) {
                 this.updateStateOfSingleSquare(arr[i].id, board);
             }
         }
     }
 
-    stateChangeHandle(id, board) {
-        //ustalamy rząd
+    findCoordinateOfSingleSquare(id) {
         let row = Math.floor(id / 8);
         let col = id % 8;
-        console.log("id:",id, "row:",row, "row:",col);
-        // ID: 14, ROW: 1 COL:6
-        // Złapać cały rząd
-        let rowToCheck = this.returnRow(row,board);
-        // złapać całą kolumne
-        let colToCheck = this.returnCol(col,board);
-        // złapać skos 1
-        let xOneToCheck = this.returnXOne(id,board);
-        // złapać skos 2
-        let xTwoToCheck = this.returnXTwo(id,board);
+        // console.log("id:",id, "row:",row, "row:",col);
+        return [row, col]
+    }
 
-        // Sprawdzanie zwróconych tabilc. 
+    // getting data in arr and resolving new state of squares
+    stateChangeHandle(id, board) {
+        const row = this.findCoordinateOfSingleSquare(id)[0]; //TODO refactor this !!!
+        const col = this.findCoordinateOfSingleSquare(id)[1];
+        let rowToCheck = this.returnRow(row, board);
+        let colToCheck = this.returnCol(col, board);
+        let xOneToCheck = this.returnXOne(id, board);
+        let xTwoToCheck = this.returnXTwo(id, board);
+
         this.squareNewStateFinder(rowToCheck, board);
         this.squareNewStateFinder(colToCheck, board);
         this.squareNewStateFinder(xOneToCheck, board);
         this.squareNewStateFinder(xTwoToCheck, board);
     }
 
-    returnRow(num, board){
+    //function to finding squars to be check 1
+    returnRow(num, board) {
         let result = [];
         let x = 8 * num;
-        for(let i = x; i<8+x; i++){
-            result.push({id: i ,value: board[i] });
+        for (let i = x; i < 8 + x; i++) {
+            result.push({id: i, value: board[i]});
         }
+        // console.log("returnRow",result);
         return result;
     }
 
-    returnCol(num, board){
+    //function to finding squars to be check 2
+    returnCol(num, board) {
         let result = [];
-        for(let i = num; i<48; i+=8){
-            result.push({id: i ,value: board[i] });
+        for (let i = num; i < 48; i += 8) {
+            result.push({id: i, value: board[i]});
         }
+        // console.log("returnCol",result);
         return result;
     }
-    returnXOne(num, board){
-        let result = [];
-        for(let i = num; i<48; i+=7){
-            result.push({id: i ,value: board[i] });
-        }
-        return result;
-    }
-    returnXTwo(num, board){
-        let result = [];
 
+    //function to finding squars to be check 3
+    returnXOne(num, board) {
+        let result = [];
+        for (let i = num; i < 48; i += 7) {
+            result.push({id: i, value: board[i]});
+        }
+        for (let i = num; i > 0; i -= 7) {
+            result.push({id: i, value: board[i]});
+        }
+        // console.log("returnXOne",result);
+        // TODO use to check board limits findCoordinateOfSingleSquare
+        return result;
+    }
+
+    //function to finding squars to be check 4
+    returnXTwo(num, board) {
+        let result = [];
+        for (let i = num; i < 48; i += 9) {
+            result.push({id: i, value: board[i]});
+        }
+        for (let i = num; i > 0; i -= 9) {
+            result.push({id: i, value: board[i]});
+        }
+        // console.log("returnXTwo",result);
+        // TODO use to check board limits findCoordinateOfSingleSquare
         return result;
     }
 
     // **************************************************************** //
     render() {
-    const {history, stepLimit, levelID} = this.state;
+        const {history, stepLimit, levelID} = this.state;
+        const winModal = <div className="modal"> Gratulacje wygrałeś </div>;
         return (
             <div className="App ">
                 <div className="container">
@@ -146,6 +194,7 @@ export default class App extends Component {
                         levelID={levelID}
                         onClick={(i, id) => this.handleClick(i, id)}
                     />
+                    {this.state.showWinModal ? winModal : null}
                 </div>
             </div>
         );
